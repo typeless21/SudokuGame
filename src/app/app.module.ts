9411718a1459fc8145432b8413409e2dd7905931
Kristaps Grebņevs <typeless21@gmail.com>
2019-03-14 11:10:12 +0200

    Form templates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BlockComponent } from './block/block.component';
 import { BoardComponent } from './board/board.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BlockComponent,
-    BoardComponent
+    BoardComponent,
+    LoginComponent,
+    RegisterComponent,
+    ReactiveFormsModule
   ],
   imports: [
     BrowserModule
---
+++ src/app/user.ts
@@ -0,0 +1,8 @@
+export class User{
+  id: number;
+  username: string;
+  password: string;
+  highScore: number;
+  savedTime: number;      // Might change data type
+  savedGameBoard: string; // Might change data type
+}
